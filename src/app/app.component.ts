a5c00578ab00ffddece89fa90a1aad5f1eb3700d
Eleneko <miraisoncloudi@gmail.com>
2023-03-12 18:34:56 -0300

    Elaboracion calculadora y operaciones basicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  title = 'Mini Calculadora';
+  num1:number=0;
+  num2:number=0;
+  result:number=0;
+
+  add():void{
+    this.result=this.num1+this.num2;
+  }
+  substract():void{
+    this.result=this.num1-this.num2;
+  }
+  multiplication():void{
+    this.result=this.num1*this.num2;
+  }
+  division():void{
+    this.result=this.num1/this.num2;
+  }
 }
